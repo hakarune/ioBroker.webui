afa80604d1a83c032a8922716e12b6c766099fab
Ace Elliott <hakarune@gmail.com>
2022-03-16 22:01:50 -0700

    adjusting IP

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-window.iobrokerHost = '192.168.1.2';
+window.iobrokerHost = '10.1.1.10';
 window.iobrokerPort = '8082';
 window.iobrokerWebuiRootUrl = window.location.protocol + '//' + window.iobrokerHost + ':8082' + '/webui/';
 window.iobrokerSocketScriptUrl = 'http://' + window.iobrokerHost + ':' + window.iobrokerPort + '/lib/js/socket.io.js';
